1320185c5e11b7008434bd3080ec191ade99be6c
Niklas Bartz <Niklas.Bartz@contagt.com>
2016-11-03 20:36:03 +0100

    Mod: Writing username in session

--- app/src/main/java/groovey/didactic/disco/org/didacticdisco/fragments/SignInFragment.java
+++ app/src/main/java/groovey/didactic/disco/org/didacticdisco/fragments/SignInFragment.java
@@ -3,15 +3,30 @@ package groovey.didactic.disco.org.didacticdisco.fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
 
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import groovey.didactic.disco.org.didacticdisco.DiscoApplication;
 import groovey.didactic.disco.org.didacticdisco.R;
+import groovey.didactic.disco.org.didacticdisco.data.Session;
 
-public class SignInFragment extends Fragment
+public class SignInFragment extends Fragment implements TextView.OnEditorActionListener
 {
-    public static SignInFragment getInstance() {
+    @BindView(R.id.username) TextView username;
+    @Inject                  Session  session;
+
+    public static SignInFragment getInstance()
+    {
         return new SignInFragment();
     }
 
@@ -19,14 +34,52 @@ public class SignInFragment extends Fragment
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        ((DiscoApplication) getActivity().getApplicationContext()).getAppComponent().inject(this);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         StrictMode.allowThreadDiskWrites(); //TODO: StrictMode gone after dev!
         StrictMode.allowThreadDiskReads();
-        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+        ButterKnife.bind(this, view);
+        username.setOnEditorActionListener(this);
+
+        return view;
+    }
+
+    @OnClick(R.id.action_start)
+    void onClick()
+    {
+        verifyUsername();
+    }
+
+    @Override
+    public boolean onEditorAction(TextView textView, int actionID, KeyEvent key)
+    {
+        // Reset errors.
+        username.setError(null);
+
+        if (actionID == R.id.login || actionID == EditorInfo.IME_NULL)
+        {
+            verifyUsername();
+            return true;
+        }
+
+        return false;
+    }
+
+    private void verifyUsername()
+    {
+        if (TextUtils.isEmpty(username.getText()))
+        {// Check for a valid username.
+            username.setError(getString(R.string.error_field_required));
+            username.requestFocus();
+        } else
+        {
+            session.set(R.string.key_username, username.getText().toString());
+        }
     }
 }
--- app/src/main/java/groovey/didactic/disco/org/didacticdisco/injections/components/AppComponent.java
+++ app/src/main/java/groovey/didactic/disco/org/didacticdisco/injections/components/AppComponent.java
@@ -4,6 +4,7 @@ package groovey.didactic.disco.org.didacticdisco.injections.components;
 import javax.inject.Singleton;
 
 import dagger.Component;
+import groovey.didactic.disco.org.didacticdisco.fragments.SignInFragment;
 import groovey.didactic.disco.org.didacticdisco.injections.modules.AppModule;
 import groovey.didactic.disco.org.didacticdisco.injections.modules.NetModule;
 import groovey.didactic.disco.org.didacticdisco.network.ApiManager;
@@ -19,4 +20,5 @@ public interface AppComponent {
     // services
     void inject(LocationTrackerService locationTrackerService);
     void inject(ApiManager apiManager);
+    void inject(SignInFragment signInFragment);
 }
