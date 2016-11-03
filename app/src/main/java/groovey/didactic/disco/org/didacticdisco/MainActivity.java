f36cfe8e961ad5c4c0d0414db0adc4ffc3890d33
STEEEPPHHHAAAANN <stephan@contagt.com>
2016-11-03 18:39:57 +0100

    Add: Native Libs for VTM

--- app/src/main/java/groovey/didactic/disco/org/didacticdisco/MainActivity.java
+++ app/src/main/java/groovey/didactic/disco/org/didacticdisco/MainActivity.java
@@ -1,8 +1,12 @@
 package groovey.didactic.disco.org.didacticdisco;
 
+import android.app.FragmentTransaction;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import groovey.didactic.disco.org.didacticdisco.fragments.GameFragment;
 
 public class MainActivity extends FragmentActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        android.app.FragmentManager.enableDebugLogging(false);
+        /*transaction.setCustomAnimations(R.anim.card_slide_in, 0, 0,
+                                        R.anim.card_slide_out);*/
+        transaction.setTransition(FragmentTransaction.TRANSIT_NONE);
+        transaction.replace(R.id.content_frame, GameFragment.getInstance());
+        transaction.commit();
     }
 }
--- app/src/main/java/groovey/didactic/disco/org/didacticdisco/fragments/GameFragment.java
+++ app/src/main/java/groovey/didactic/disco/org/didacticdisco/fragments/GameFragment.java
@@ -1,10 +1,112 @@
 package groovey.didactic.disco.org.didacticdisco.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-/**
- * Created by sbrandt on 03.11.16.
- */
+import com.commonsware.cwac.colormixer.ColorMixer;
+import com.commonsware.cwac.colormixer.ColorMixerDialog;
 
-public class GameFragment extends Fragment {
+import org.oscim.android.MapPreferences;
+import org.oscim.android.MapView;
+import org.oscim.backend.canvas.Color;
+import org.oscim.layers.tile.vector.VectorTileLayer;
+import org.oscim.layers.tile.vector.labeling.LabelLayer;
+import org.oscim.map.Map;
+import org.oscim.map.ViewController;
+import org.oscim.theme.IRenderTheme;
+import org.oscim.theme.ThemeLoader;
+import org.oscim.theme.VtmThemes;
+import org.oscim.tiling.source.geojson.OsmLanduseJsonTileSource;
+import org.oscim.tiling.source.geojson.OsmRoadLabelJsonTileSource;
+import org.oscim.tiling.source.geojson.OsmRoadLineJsonTileSource;
+import org.oscim.tiling.source.geojson.OsmWaterJsonTileSource;
+
+import groovey.didactic.disco.org.didacticdisco.R;
+
+public class GameFragment extends Fragment implements ColorMixer.OnColorChangedListener {
+    private Map mMap;
+    private MapPreferences mPrefs;
+    private MapView mMapView = null;
+
+    public static GameFragment getInstance() {
+        return new GameFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
+            savedInstanceState)
+    {
+        StrictMode.allowThreadDiskWrites(); //TODO: StrictMode gone after dev!
+        StrictMode.allowThreadDiskReads();
+        return inflater.inflate(R.layout.gamefragment, container, false);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        setupEnvironment();
+        //new ColorMixerDialog(getActivity(), Color.WHITE, this).show();
+
+    }
+
+    public void onPause() {
+        super.onPause();
+    }
+
+    private void setupEnvironment() {
+
+        mMapView = (MapView)getActivity().findViewById(R.id.vtmMapView);
+        mMap = mMapView.map();
+        mPrefs = new MapPreferences(this.getClass().getName(), getActivity().getApplicationContext());
+        //setup map
+        ViewController vc = mMap.viewport();
+        vc.setTilt(0);
+        vc.setMaxTilt(65);
+        vc.setMaxZoomLevel(21);
+
+        IRenderTheme theme = ThemeLoader.load(VtmThemes.DEFAULT);
+
+        mMap.setBaseMap(new VectorTileLayer(mMap, new OsmLanduseJsonTileSource()));
+        mMap.setTheme(theme);
+
+        VectorTileLayer l = new VectorTileLayer(mMap, new OsmWaterJsonTileSource());
+        l.setRenderTheme(theme);
+        l.tileRenderer().setOverdrawColor(0);
+        mMap.layers().add(l);
+
+        l = new VectorTileLayer(mMap, new OsmRoadLineJsonTileSource());
+        l.setRenderTheme(theme);
+        l.tileRenderer().setOverdrawColor(0);
+        mMap.layers().add(l);
+
+        l = new VectorTileLayer(mMap, new OsmRoadLabelJsonTileSource());
+        l.setRenderTheme(theme);
+        l.tileRenderer().setOverdrawColor(0);
+        mMap.layers().add(l);
+        mMap.layers().add(new LabelLayer(mMap, l));
+
+
+        /*l = new VectorTileLayer(mMap, new OsmBuildingJsonTileSource());
+        l.setRenderTheme(theme);
+        l.tileRenderer().setOverdrawColor(0);
+        mMap.layers().add(l);
+        mMap.layers().add(new BuildingLayer(mMap, l));*/
+        mPrefs.clear();
+    }
+
+    @Override
+    public void onColorChange(int i) {
+
+    }
 }
